c3012878ed7e6fc1f2afa2a2d19a082ff8a65029
Mohamed Hussiani <mohamedhussiani@outlook.com>
2025-03-08 23:17:09 +0000

    completed module 5 lesson 5

--- M05.turn_on_lights_workbook/lesson/player_fps_controller.gd
+++ M05.turn_on_lights_workbook/lesson/player_fps_controller.gd
@@ -6,6 +6,7 @@ class_name PlayerFPSController extends CharacterBody3D
 @export_category("Ground Movement")
 @export_range(1.0, 10.0, 0.1) var max_speed_jog := 4.0
 @export_range(1.0, 15.0, 0.1) var max_speed_sprint := 7.0
+@export_range(1.0, 10.0, 0.1) var max_speed_crouch := 2.0
 @export_range(1.0, 100.0, 0.1) var acceleration_jog := 15.0
 @export_range(1.0, 100.0, 0.1) var acceleration_sprint := 25.0
 @export_range(1.0, 100.0, 0.1) var deceleration := 12.0
@@ -15,9 +16,15 @@ class_name PlayerFPSController extends CharacterBody3D
 @export_range(1.0, 50.0, 0.1) var max_fall_speed := 20.0
 @export_range(1.0, 20.0, 0.1) var jump_velocity := 8.0
 
+var is_crouching := false: set = set_is_crounching
+
 @onready var _camera: Camera3D = %Camera3D
-@onready var _neck: Node3D = %Neck
+@onready var _neck: Node3D = %Neck 
 @onready var _neck_start_height: float = _neck.position.y
+@onready var _collision_shape_3d: CollisionShape3D = %CollisionShape3D
+@onready var _crounch_ceiling_cast: ShapeCast3D = %CrounchCeilingCast
+
+@onready var _collision_shape_start_height: float = _collision_shape_3d.shape.height # = height of the initial player collision
 
 func _process(delta: float) -> void:
 	var joystick_look_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -40,12 +47,18 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_pressed("sprint"):
 			max_speed = max_speed_sprint
 			acceleration = acceleration_sprint
+		if is_crouching: # when moving and crouch button pressed
+			max_speed = max_speed_crouch
 		
 		velocity_change = acceleration * delta # update velocity data
 		velocity_ground_plane = velocity_ground_plane.move_toward(movement_direction_3d * max_speed, velocity_change)
 	else: # decelerating to stop
 		velocity_change = deceleration * delta
 		velocity_ground_plane = velocity_ground_plane.move_toward(Vector3.ZERO, velocity_change)
+	
+	# pass in the state of when crouch button is pressed or not, to is_crouching setter
+	if is_on_floor():
+		set_is_crounching(Input.is_action_pressed("crouching"))
 	
 	velocity.x = velocity_ground_plane.x
 	velocity.z = velocity_ground_plane.z
@@ -71,7 +84,6 @@ func _physics_process(delta: float) -> void:
 		# line below: will decrease the camera y position of neck by 0.1
 		impact_tween.tween_property(_neck, "position:y", _neck.position.y - 0.2 * impact_intensity, 0.06)
 		impact_tween.tween_property(_neck, "position:y", _neck_start_height, 0.1) # back to orginal
-	
 
 func _unhandled_input(event: InputEvent) -> void:
 	var is_mouse_button := event is InputEventMouseButton
@@ -86,7 +98,40 @@ func _unhandled_input(event: InputEvent) -> void:
 	if (event is InputEventMouseMotion and is_mouse_captured):
 		var look_offset_2d: Vector2 = event.screen_relative * mouse_sensitivity
 		_rotate_camera_by(look_offset_2d)
+
+func set_is_crounching(new_value: bool) -> void:
+	if is_crouching == new_value:
+		return # prevent any changes if crouching state is unchanged
+
+	# on when player is no longer pressing crouch button,
+	# check if player node has room to stop crouching above, otherwise return
+	if new_value == false: # stopped pressing crouch button
+		_crounch_ceiling_cast.force_shapecast_update()
+		if _crounch_ceiling_cast.is_colliding():
+			return
 	
+	is_crouching = new_value
+	
+	# when crouching, half the collision shape height, otherwise reset it back to default value
+	if is_crouching:
+		_collision_shape_3d.shape.height = _collision_shape_start_height / 2
+	else:
+		_collision_shape_3d.shape.height = _collision_shape_start_height
+		
+	# the vertcal or y value must be half of whatever the collision shape height is
+	_collision_shape_3d.position.y = _collision_shape_3d.shape.height / 2
+	
+	# create a variable to store target height
+	var neck_target_height := 0.0
+	if is_crouching: # update viduals
+		neck_target_height = _neck_start_height * 0.7 # target neck height will be at 70% the start
+	else:
+		neck_target_height = _neck_start_height # otherwise reset to start
+	
+	# tween to target neck height in 0.25 sec, trans_back and ease_out
+	var crouch_tween = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	crouch_tween.tween_property(_neck, "position:y", neck_target_height, 0.25)
+
 func _rotate_camera_by(look_offset_2d: Vector2) -> void:
 	#how to rotate left and right
 	_camera.rotation.y -= look_offset_2d.x
